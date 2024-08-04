0df62fc6cb31e433d6a2fab89b1509077760c506
pablosb01 <pablosb01@icloud.com>
2024-08-04 17:29:44 +0200

    name rotator

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,4 +1,7 @@
-import { inter, roboto_mono, bebas_neue } from "../../app/fonts";
+"use client";
+
+import NameRotator from "../NameRotator/NameRotator";
+import { artistas } from './Artistas.js'
 
 export default function Video() {
   return (
@@ -8,7 +11,7 @@ export default function Video() {
           <iframe
               width="720"
               height="480"
-              src={`https://www.youtube.com/embed/sT4lpLhzUYU?si=3TLGc-FuXAlYXauE?enablejsapi=1&autoplay=1&mute=1&loop=1&controls=0&origin=https&fullscreen=1`}
+              src={`https://www.youtube.com/embed/sT4lpLhzUYU?si=3TLGc-FuXAlYXauE?enablejsapi=1&autoplay=1&mute=1&loop=1&controls=0&origin=https&fullscreen=1&playlist=sT4lpLhzUYU`}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               className="relative top-0 left-0 video-embed overflow-hidden"
@@ -17,10 +20,15 @@ export default function Video() {
           ></iframe>
         </div>
       {/* Aquí puedes agregar más contenido superpuesto si lo deseas */}
-      <div className="relative z-10 flex flex-col items-center justify-center w-full h-full text-white text-center">
-        <h1 className="text-4xl font-bold">14·15·16·17·18 AGO 2024</h1>
-        <h1 className="text-6xl font-bold">27<sup className="text-4xl">th</sup>E D I T I O N</h1>
-        <h1 className={`text-3xl font-bold`}>A R R I O N D A S ,  C A N G A S D E O N I S</h1>
+      <div className="relative z-10 gap-2 flex flex-col items-center justify-center w-full h-full text-white text-center">
+        <h1 className="text-4xl font-bold tracking-wide">14·15·16·17·18 AGO 2024</h1>
+        <div className="text-6xl gap-2 flex-row flex font-bold">
+          <h1>27</h1>
+          <sup className="text-2xl">th</sup>
+          <h1 className='tracking-wider'>EDITION</h1>
+        </div>
+        <h1 className={`text-4xl font-bold tracking-wider`}>ARRIONDAS ,  CANGAS DE ONIS</h1>
+        <NameRotator names={artistas}/>
       </div>
     </div>
     </>
