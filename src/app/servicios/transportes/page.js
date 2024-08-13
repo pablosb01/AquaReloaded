7e81f98681f8ae1a3defee1dd37a6d80390f9be7
tonimtnz <tonimartinezlluesma@gmail.com>
2024-08-13 11:20:41 +0200

    tour y transportes + done que Al Capone

--- src/app/servicios/transportes/page.js
+++ src/app/servicios/transportes/page.js
@@ -1,5 +1,55 @@
+import { bebas_neue, roboto } from "@/app/fonts";
+
 export default function transportes() {
     return(
-        <p>Transporte</p>
+        <main className={`w-full m-w-[2500px] min-h-[80vh] h-full ${roboto.className}`}>
+            <div className="flex flex-col m-w-[2500px] w-full items-center py-8">
+                <div className="lg:w-[70%] w-[90%] flex flex-col gap-12 items-center">
+                <h2 className={`text-center text-6xl font-bold ${bebas_neue.className} sombrita`}><a href="#lanzaderas">LANZADERAS</a> / <a href="#buses">AUTOBUSES</a></h2>
+                    <div className="flex flex-col items-center w-full gap-4 text-[18px]">
+                        <img src="https://aquasella.com/wp-content/uploads/2024/06/lanzaderas-web-1024x773.png" className="w-[80%]" id="lanzaderas"/>
+                        <div className="flex flex-col items-center gap-4 w-[85%]">
+                            <h4 className="sombrita-tres font-bold text-2xl text-center">Lanzaderas Aquasella – Te llevamos al festival 💪</h4>
+                            <p>Si te alojas en Cangas de Onís o alrededores, o simplemente quieres dejar el coche aparcado en Cangas de Onís por comodidad, ya puedes llegar al Prau con nuestro <strong className="text-rose-500">SERVICIO DE LANZADERAS </strong>Aquasella 2024. De esta forma podrás llegar y marcharte del festi de una forma más rápida, segura y sostenible.</p>
+                            <p>Además, si estás alojado con tu Autocaravana en el Parking externo al festival que hemos habilitado, dispondrás de una parada muy próxima a este recinto.</p>
+                            <h4 className="sombrita-tres font-bold text-2xl pb-4">HORARIOS</h4>
+                            <div className="flex justify-between flex-wrap gap-6 text-center border-2 border-rose-500 p-4">
+                                <div className="flex flex-col gap-2 w-full lg:w-[40%]">
+                                    <h4 className="text-center text-rose-500 font-bold">Miércoles 14 Agosto</h4>
+                                    <p>Salidas: Desde las 20:00h hasta las 23:00h</p>
+                                    <p>Retornos (jueves): Desde las 00:00h hasta las 02:00h</p>
+                                </div>
+                                <div className="flex flex-col gap-2 w-full lg:w-[40%]">
+                                    <h4 className="text-center text-rose-500 font-bold">Jueves 15 Agosto</h4>
+                                    <p>Salidas: Desde las 19:00h hasta las 23:00h</p>
+                                    <p>Retornos (viernes): Desde las 05:00h hasta las 07:00h</p>
+                                </div>
+                                <div className="flex flex-col gap-2 w-full lg:w-[40%]">
+                                    <h4 className="text-center text-rose-500 font-bold">Viernes 16 Agosto</h4>
+                                    <p>Salidas: Desde las 19h hasta las 23h</p>
+                                    <p>Retornos (sábado): Desde las 08:00h hasta las 10:00h</p>
+                                </div>
+                                <div className="flex flex-col gap-2 w-full lg:w-[40%]">
+                                    <h4 className="text-center text-rose-500 font-bold">Sábado 17 Agosto</h4>
+                                    <p>Salidas: Desde las 19h hasta las 23h</p>
+                                    <p>Retornos (domingo): Desde las 10h hasta las 18h</p>
+                                </div>
+                            </div>
+                            <p className={`text-2xl text-amber-500 text-bold ${bebas_neue.className} text-center`}>SALIDAS desde la Estación de Autobuses de Cangas de Onís.</p>
+                            <p className={`text-2xl text-amber-500 text-bold ${bebas_neue.className} text-center`}>RETORNOS desde el recinto de AQUASELLA.</p>
+                            <p className={`text-2xl text-rose-500 text-bold ${bebas_neue.className} text-center`}>* Este Servicio es exclusivo para los asistentes al festival. Se exigirá abono o entrada de día para acceder a las lanzaderas.</p>
+                        </div>
+                        <img src="https://aquasella.com/wp-content/uploads/2024/03/2024.03.13_AQSPOST-825x1024.png" className="w-[80%] pt-12" id="buses"/>
+                        <div className="flex flex-col gap-6 w-[85%] text-left pb-12">
+                            <p>Como cada edición, dispondremos de un Bus Oficial Aquasella con diversos puntos de salida desde todo el territorio nacional.</p>
+                            <p>En el siguiente listado puedes consultar las ciudades de origen y destino de las diferentes rutas.</p>
+                            <p>Oviedo · Gijón · Santander · León · Bilbao · Burgos · Vitoria · Palencia · Zamora · Lugo · San Sebastián · Valladolid · A Coruña · Logroño · Salamanca · Pamplona · Santiago · Ourense · Ávila · Vigo · Madrid · Zaragoza · Toledo · Cáceres · Merida · Badajoz · Tarragona · Barcelona · Castellón · Valencia · Sevilla</p>
+                            <p><strong>Si tu ciudad no está en este listado</strong> o tienes cualquier duda respecto a los buses, escribe a <strong className="text-rose-500">aquasella@viajexpres.com o llama/WhatsApp al 676 680 774.</strong></p>
+                        </div>
+                        <iframe src="https://venta.enterticket.es/buy/?id=34319&amp;color=f43f5e" id="etktfrm34319" width="100%" frameborder="0" className="lg:h-[3500px] md:h-[4000px] h-[4300px]"></iframe>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
--- src/app/tour/page.js
+++ src/app/tour/page.js
@@ -1,5 +1,46 @@
+import SliderTour from "@/components/SliderTour/SliderTour";
+import { bebas_neue, roboto } from "../fonts";
+
 export default function tour() {
     return (
-        <p>Pagina del Tour</p>
+        <main className={`w-full m-w-[2500px] min-h-[80vh] h-full ${roboto.className}`}>
+            <div className="flex flex-col m-w-[2500px] w-full items-center py-8">
+                <div className="lg:w-[70%] w-[90%] flex flex-col gap-12 items-center">
+                    <h2 className={`text-center text-6xl font-bold ${bebas_neue.className} sombrita`}>AQUASELLA ON THE ROAD 2024</h2>
+                        <div className="flex flex-col items-center w-full gap-12 text-[18px]">
+                            <img src="https://aquasella.com/wp-content/uploads/2024/03/imagen-principal-tour.jpg" className="w-[90%]"/>
+                            <div className="flex flex-col gap-4 w-[90%]">
+                                <p>En cada edición y con la intención de generar sinergias con los promotores de salas y conectar directamente con nuestro público, el festival realiza una serie de presentaciones oficiales en los club y eventos más representativos de música electrónica de nuestro país.</p>
+                                <p>En este 2024, el tour de presentaciones tendrá lugar entre los meses de marzo y agosto. Nuestro objetivo es llevar la experiencia Aquasella a todas las ciudades de España y haceros partícipes de este gran proyecto.</p>
+                                <p>En los últimos tours en 2019, 2022 y 2023 visitamos más de 20 ciudades y clubes emblemáticos a lo largo del territorio español, llegando a varias ciudades de Francia y Portugal.</p>
+                                <p>Además de todo esto, hemos repartido cientos de regalos y sorteado abonos que muchos Aquaseller@s han podido disfrutar a lo largo de estos años.</p>
+                                <p>Después de este tour apasionante por todo el país, estaremos listos y deseosos de recibiros en la 27 Ed de Aquasella festival.</p>
+                                <p>El festival se celebra este verano los días 14, 15, 16, 17 y 18 de agosto en los alrededores de la localidad asturiana de Arriondas, aunque el recinto se encuentra en el término municipal de Cangas de Onís.</p>
+                            </div>
+                            <div className="w-full">
+                                <SliderTour/>
+                            </div>
+                            <div className="">
+                                <h2 className={`text-center text-6xl font-bold ${bebas_neue.className} sombrita tracking-wide`}>FORMA PARTE DEL</h2>
+                                <h2 className={`text-center text-8xl font-bold ${bebas_neue.className} sombrita tracking-wider`}>TOUR OFICIAL 2024</h2>
+                            </div>
+                            <div className="flex flex-col gap-6 items-center">
+                                <p className={`text-center ${bebas_neue.className} text-2xl text-rose-500`}>Si después de todo lo que te hemos contado, te han entrado ganas de disfrutar este viaje con nosotros, te ofrecemos todas estas cosas:</p>
+                                <div className="sombrita-tres font-bold text-center flex flex-col gap-2">
+                                    <p>Plantillas para cartelería</p>
+                                    <p>Sorteo de ABONO DOBLE VIP + CAMPING</p>
+                                    <p>Regalos de merchandising</p>
+                                    <p>Visuales Aquasella</p>
+                                    <p>Contratación de artistas nacionales / internacionales</p>
+                                    <p>Promoción del tour en redes sociales + newsletters</p>
+                                    <p>Promoción de cada evento en redes de Peacock Agency</p>
+                                    <p>El promotor recibirá dos ABONOS VIP de regalo</p>
+                                </div>
+                                <img src="https://aquasella.com/wp-content/uploads/2023/03/faldon.png" className="w-full py-12"/>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </main> 
     )
 }
