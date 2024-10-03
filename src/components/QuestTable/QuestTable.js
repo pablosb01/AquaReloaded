1e60e3e697876801f432d7a2086ac7347adcebcf
Pablo Diaz <pablosb01@icloud.com>
2024-10-03 10:25:49 +0200

    jsons

--- src/components/QuestTable/QuestTable.js
+++ src/components/QuestTable/QuestTable.js
@@ -2,34 +2,17 @@
 
 import Test from './Test';
 import TestArray from '../Test/TestArray';
+import TestJSON from "../Test/TestJSON.json"
 
 function QuestTable() {
 
 
   return (
     <div className='w-full h-full border-2 border-rose-500 text-center'>
-      {TestArray.map((item, index) => (
+      {TestJSON.map((item, index) => (
           <Test key={index} object={item}/>
       ))}
     </div>
-    /* <div>
-      <details open={openDetail === 0} onToggle={() => handleToggle(0)}>
-        <summary>Detalle 1</summary>
-        <p>Contenido del detalle 1.</p>
-      </details>
-
-      <details open={openDetail === 1} onToggle={() => handleToggle(1)}>
-        <summary>Detalle 2</summary>
-        <p>Contenido del detalle 2.</p>
-      </details>
-
-      <details open={openDetail === 2} onToggle={() => handleToggle(2)}>
-        <summary>Detalle 3</summary>
-        <p>Contenido del detalle 3.</p>
-      </details>
-    </div> */
-  
-
   );
 }
 
--- src/components/QuestTable/QuestTableLock.js
+++ src/components/QuestTable/QuestTableLock.js
@@ -2,12 +2,13 @@
 
 import Test from "./Test";
 import TestLockersArray from "../Test/TestLockersArray";
+import LockersJSON from "../Test/LockersJSON"
 
 function QuestTableLock(){
 
     return (
         <div className='w-full h-full border-2 border-rose-500 text-center'>
-      {TestLockersArray.map((item, index) => (
+      {LockersJSON.map((item, index) => (
           <Test key={index} object={item}/>
       ))}
     </div>
